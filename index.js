7cab4fa7d784894443c594996711c071398e7f88
Suplob <parbaroy30@gmail.com>
2022-02-03 18:17:22 +0600

    making changes

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.send("Server side of tour planner");
+  res.send("Server side of tour planner askpdsas");
 });
 
 async function run() {
